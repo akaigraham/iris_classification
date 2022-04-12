116e5b8140943834a8771675a399277f8e45395d
Addington Graham <akaigraham@gmail.com>
2022-04-12 18:27:22 -0400

    removed files

--- request.py
+++
@@ -1,7 +0,0 @@
-# request.py requests the server for the predictions
-
-# import libraries
-import requests
-url = 'http://localhost:5000/api'
-r = requests.post(url, json={'measurements':[1,1,1,1]})
-print(r.json())
